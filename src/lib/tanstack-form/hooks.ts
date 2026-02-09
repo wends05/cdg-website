5e5faee7aa59a0104f3aefd3cef1daa30314c402
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-02-10 00:24:27 +0800

    feat: added tanstack form hooks

---
+++ src/lib/tanstack-form/hooks.ts
@@ -0,0 +1,11 @@
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+export const { fieldContext, formContext } = createFormHookContexts();
+
+// Allow us to bind components to the form to keep type safety but reduce production boilerplate
+// Define this once to have a generator of consistent form instances throughout your app
+export const { useAppForm } = createFormHook({
+  fieldComponents: {},
+  formComponents: {},
+  fieldContext,
+  formContext,
+});
