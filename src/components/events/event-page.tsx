3f3a7a8b29334b22414097c0d96282cd8e404710
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-02-10 10:08:09 +0800

    feat: started on event page

---
+++ src/components/events/event-page.tsx
@@ -0,0 +1,11 @@
+import type { EventRecord } from "@/types/domain";
+
+interface EventPageProps {
+	event: EventRecord;
+}
+
+export default function EventPage({ event }: EventPageProps) {
+	return <div>
+    
+  </div>;
+}
