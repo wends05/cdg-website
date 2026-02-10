64bee2fcf92fcefe1639b4ac5921f495077ef883
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2026-02-11 01:14:05 +0800

    feat: added an event page with back button, image rendering, and details rendered from markdown data. added a "get certificate" portion to navigate to the certificates page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hhl5wcanr4.ufs.sh",
+        pathname: "/f/*"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
